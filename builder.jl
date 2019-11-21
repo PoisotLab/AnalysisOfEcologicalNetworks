66de350573847757dd807324126ff4bcabfbe56d
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-11-21 11:35:11 -0500

    fix upper version issue with Weave

--- builder.jl
+++ builder.jl
@@ -5,10 +5,13 @@ using Weave
 
 chapters_ = "chapters"
 
+ispath(joinpath("dist", chapters_)) || mkdir(joinpath("dist", chapters_))
+ispath(joinpath("dist", "notebooks")) || mkdir(joinpath("dist", "notebooks"))
+
 chapters_content = joinpath.(chapters_, readdir(chapters_))
 filter!(isfile, chapters_content)
 filter!(p -> endswith(p, ".Jmd"), chapters_content)
 
 for chapter in chapters_content
-    weave(chapter, out_path="dist", doctype="github")
+    weave(chapter, out_path=joinpath("dist", chapters_), doctype="github")
 end
