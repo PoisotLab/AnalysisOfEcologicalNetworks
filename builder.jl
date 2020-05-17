648a3ccac901199e0c2a9e898138ce959c7fe032
Timothée <timothee.poisot@umontreal.ca>
2020-05-16 21:51:07 -0400

    latex move

--- builder.jl
+++ builder.jl
@@ -1,20 +1,32 @@
 using Weave
 
 chapters_ = "chapters"
-notebooks_ = "notebooks"
-pdf_ = "pdf"
 
 ispath("content") || mkdir("content")
-for output in [chapters_, notebooks_, pdf_]
-    ispath(joinpath("content", output)) || mkdir(joinpath("content", output))
+
+chapters_folders = joinpath.(chapters_, readdir(chapters_))
+
+all_files = String[]
+
+for chapter_folder in chapters_folders
+    global all_files
+    chapter_files = joinpath.(chapter_folder, readdir(chapter_folder))
+    filter!(f -> endswith(f, "text.texw"), chapter_files)
+    if length(chapter_files) == 1
+        @info "Weaving $(chapter_folder)"
+        chapter_text = first(chapter_files)
+        weave(chapter_text)
+        push!(all_files, replace(chapter_text, ".texw"=>".tex"))
+    end
 end
 
-chapters_content = joinpath.(chapters_, readdir(chapters_))
-filter!(isfile, chapters_content)
-filter!(p -> endswith(p, ".Jmd"), chapters_content)
+include_string = """
+"""
+for chapter_file in all_files
+    global include_string
+    include_string *= "\n\t\\input{../$(replace(chapter_file, "\\"=>"/"))}%"
+end
 
-for chapter in chapters_content
-    root_name = first(split(last(split(chapter, "/")), ".Jmd"))
-    @info "Weaving $(root_name)"
-    weave(chapter, out_path=joinpath("content", chapters_))
+open(joinpath("template", "chapters.tex"), "w") do f
+    write(f, include_string)
 end
